e4a7a8515dbbe1e003f25a459f2c08e344cf15dd
GarrettSorel <garrettsorel@gmail.com>
2019-04-19 12:48:05 -0400

    completed skeleton logic need css

--- js/script.js
+++ js/script.js
@@ -1,16 +1,15 @@
 const grid = document.querySelector('#container');
 
-let createGrid = window.onload = function() {
-  let create = prompt('Input the number of rows and columns.');
-  let number = parseInt(create);
-
-  for (i = 0; i < number; i++) {
+function createGrid (num) {
+  for (i = 0; i < num; i++) {
     let row = document.createElement('div');
     row.classList.add('row');
 
-    for (r = 0; r < number; r++) {
+    for (r = 0; r < num; r++) {
       let box = document.createElement('div');
       box.classList.add('grid-square');
+      box.style.height = (960 / num) + "px";
+      box.style.width = (960 / num) + "px";
       row.appendChild(box);
     }
     grid.appendChild(row);
@@ -19,10 +18,26 @@ let createGrid = window.onload = function() {
 }
 
 function changeColor() {
-let getBox = document.querySelectorAll('.grid-square');
-getBox.forEach((box) => {
-  box.addEventListener("mouseover", (e) => {
-  e.target.style.backgroundColor = "white";
-});
-});
+  let getBox = document.querySelectorAll('.grid-square');
+    getBox.forEach((box) => {
+    box.addEventListener("mouseover", (e) => {
+      e.target.style.backgroundColor = "white";
+    });
+    });
 }
+
+function resetGrid () {
+  let deleteRows = document.querySelectorAll('.row');
+
+  for (i = 0; i < deleteRows.length; i++) {
+    let deleteRow = document.querySelector('.row');
+    container.removeChild(deleteRow);
+  }
+
+
+  let create = prompt('Input the number of rows and columns.');
+  let number = parseInt(create);
+  createGrid(number);
+}
+
+window.onload = createGrid(16);
